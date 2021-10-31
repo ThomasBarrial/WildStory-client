8498cdc571623bf8529b79df9c3decea6ae172d2
Thomas <thomas@barrial.fr>
2021-10-31 12:08:56 +0100

    Add colors and font

--- src/views/Layout.tsx
+++ src/views/Layout.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import logo from '../assets/logo.svg';
+import logo from '../assets/logo.png';
 
 function Layout(): JSX.Element {
   return (
-    <div className="h-screen w-screen flex flex-col items-center justify-center text-5xl bg-darkBlue font-lexend text-white font-bold">
-      <img className="" src={logo} alt="" />
+    <div className="h-screen w-screen bg-black flex flex-col items-center justify-center text-5xl bg-darkBlue font-lexend text-white font-bold">
+      <img src={logo} alt="" />
     </div>
   );
 }
