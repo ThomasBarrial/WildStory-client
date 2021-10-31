89e733666df5b66cebda86ed918733e94a98bbe9
Thomas <thomas@barrial.fr>
2021-10-31 11:37:28 +0100

    Install axios and reactQuery

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/index.css';
-import { ApolloProvider } from '@apollo/client';
+
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import { store } from './store';
-import { client } from './API/client';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <Provider store={store}>
-        <Router>
-          <App />
-        </Router>
-      </Provider>
-    </ApolloProvider>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
