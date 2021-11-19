725ecc5b03cb77f0b1279b7092ad41b51c8da078
Thomas <thomas@barrial.fr>
2021-11-19 23:33:29 +0100

    ouai

--- src/components/post/CreateUpdatePost.tsx
+++ src/components/post/CreateUpdatePost.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function CreateUpdatePost(): JSX.Element {
   return (
     <div>
-      <p className="text-white">Create Post</p>
+      <p className="text-white">Create Post OUUUUUAI</p>
     </div>
   );
 }
