a623c6ff9ed1c318f643c214c92ab13b4c2bfb31
Thomas Barrial <thomas.barrial@gmail.com>
2022-02-27 15:12:04 +0100

    add react

--- src/components/post/ImageSlider.tsx
+++ src/components/post/ImageSlider.tsx
@@ -1,5 +1,8 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import { responsive } from '../../style/responsiveCaroussel';
 
 interface IProps {
   item: IPost;
@@ -8,20 +11,30 @@ interface IProps {
 function ImageSlider({ item }: IProps): JSX.Element {
   return (
     <div className="flex flex-col overflow-x-scroll lg:w-full mb-5">
-      {item.imageUrl.map((image) => {
-        return (
-          <div
-            key={image}
-            className="w-screen h-80 lg:rounded-md md:h-tablet xl:h-desktop md:w-full "
-            style={{
-              backgroundImage: `url(${image})`,
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat',
-              backgroundSize: 'cover',
-            }}
-          />
-        );
-      })}
+      <Carousel
+        // eslint-disable-next-line no-console
+        ssr
+        className="scroll-snap-x w-full"
+        responsive={responsive}
+        showDots={false}
+        swipeable
+        draggable
+      >
+        {item.imageUrl.map((image) => {
+          return (
+            <div
+              key={image}
+              className="w-screen h-80 lg:rounded-md md:h-tablet xl:h-desktop md:w-full "
+              style={{
+                backgroundImage: `url(${image})`,
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+              }}
+            />
+          );
+        })}
+      </Carousel>
     </div>
   );
 }
